5af0cc784933977e8b6294f89f52fce1b1eb4714
Sergei Matros <sergei@matros.com.ua>
2019-04-27 17:45:11 +0300

    Token validation, MyTasks action (dummy)

--- Api.php
+++ Api.php
@@ -1,9 +1,52 @@
 <?php declare(strict_types=1);
 
+require_once 'Exceptions/NotFoundException.php';
+require_once 'Exceptions/UnauthorizedException.php';
 
 class Api {
 
     private const API_TOKEN_SALT = 'MyLittleSaltHere2019';
+
+    private static $tokenArray = [
+        'd98bc7701f03aca772b2f00921daa42e8904d87a' => [
+            'userId' => 11,
+            'expireAt' => 1556369432, // expired
+        ],
+        'ba66df5d449a1318a16baef6f10be41d0a7c5d3b' => [
+            'userId' => 11,
+            'expireAt' => 1656369432, // valid
+        ],
+        '1dcc07cbffafba6122153ddee83855b6120b3ece' => [
+            'userId' => 99,
+            'expireAt' => 1756369432, // valid
+        ],
+    ];
+
+    private static $taskArray = [
+        '11' => [
+            [
+                'id' => 10,
+                'title' => 'Finish Trial task',
+                'status' => 'active',
+                'priority' => 'high',
+                'dueDate' => 1556189059,
+            ],
+            [
+                'id' => 23,
+                'title' => 'Fix my watch',
+                'status' => 'active',
+                'priority' => 'low',
+                'dueDate' => 1556199001,
+            ],
+            [
+                'id' => 9,
+                'title' => 'Find a new Job',
+                'status' => 'done',
+                'priority' => 'high',
+                'dueDate' => 1553145057,
+            ]
+        ]
+    ];
 
     public function init(): void
     {
@@ -13,36 +56,39 @@ class Api {
     private function router(): void
     {
         $route = $_SERVER['PATH_INFO'] ?? '';
-        $token = $_REQUEST['token'] ?? '';
-        switch ($route) {
-            case '/v1/login':
-                $email = $_REQUEST['email'] ?? '';
-                $password = $_REQUEST['password'] ?? '';
-                $this->loginAction($email, $password);
-                break;
-            case '/v1/register':
-                $email = $_REQUEST['email'] ?? '';
-                $password = $_REQUEST['password'] ?? '';
-                $this->registerAction($email, $password);
-                break;
-            case '/v1/myTasks':
-                $this->validateToken($token);
-                $this->getMyTasksAction();
-                break;
-            case '/v1/createTask':
-                $this->validateToken($token);
-                $this->createTaskAction();
-                break;
-            case '/v1/markAsDone':
-                $this->validateToken($token);
-                $this->markAsDoneAction();
-                break;
-            case '/v1/deleteTask':
-                $this->validateToken($token);
-                $this->deleteTaskAction();
-                break;
-            default:
-                $this->unknownAction();
+        $token = $_SERVER['HTTP_TOKEN'] ?? '';
+        $email = $_REQUEST['email'] ?? '';
+        $password = $_REQUEST['password'] ?? '';
+        try {
+            switch ($route) {
+                case '/v1/login':
+                    $this->loginAction($email, $password);
+                    break;
+                case '/v1/register':
+                    $this->registerAction($email, $password);
+                    break;
+                case '/v1/myTasks':
+                    $this->validateToken($token);
+                    $userId = $this->getUserIdByToken($token);
+                    $this->getMyTasksAction($userId);
+                    break;
+                case '/v1/createTask':
+                    $this->validateToken($token);
+                    $this->createTaskAction();
+                    break;
+                case '/v1/markAsDone':
+                    $this->validateToken($token);
+                    $this->markAsDoneAction();
+                    break;
+                case '/v1/deleteTask':
+                    $this->validateToken($token);
+                    $this->deleteTaskAction();
+                    break;
+                default:
+                    $this->unknownAction();
+            }
+        } catch (\Exception $e) {
+            $this->internalServerErrorResponse($e->getMessage());
         }
     }
 
@@ -52,8 +98,15 @@ class Api {
      */
     public function loginAction(string $email, string $password): void
     {
-        $token = $this->getToken($email, $password);
-        $this->formatResponse(['data' => $token]);
+        try {
+            $userId = $this->getUserId($email, $password);
+            $token = $this->getTokenByUserId($userId);
+            $this->formatResponse(['data' => $token]);
+        } catch (\Exception $e) {
+            $this->internalServerErrorResponse($e->getMessage());
+        }
+//        $token = $this->getToken($email, $password);
+//        $this->formatResponse(['data' => $token]);
     }
 
     public function registerAction(string $email, string $password): void
@@ -62,10 +115,11 @@ class Api {
         $this->formatResponse(['data' => 'register action']);
     }
 
-    public function getMyTasksAction(): void
+    public function getMyTasksAction(int $userId): void
     {
+        $userTaskArray = $this->taskArray[$userId] ?? [];
         // TODO: implement
-        $this->formatResponse(['data' => 'get my tasks action']);
+        $this->formatResponse(['data' => $userTaskArray]);
     }
 
     public function createTaskAction(): void
@@ -88,12 +142,41 @@ class Api {
 
     /**
      * @param string $token
-     * @return bool
+     * @return void
+     * @throws UnauthorizedException
      */
-    private function validateToken(string $token): bool
+    private function validateToken(string $token): void
     {
-        // TODO: implement
-        return true;
+        if ($token === '') {
+            throw new UnauthorizedException('Token is empty');
+        }
+        $tokenData = $this->tokenArray[$token] ?? [];
+        if (\count($tokenData) === 0) {
+            throw new UnauthorizedException('Token was not found');
+        }
+        $expireAt = $tokenData['expireAt'] ?? 0;
+        if (time() > $expireAt) {
+            throw new UnauthorizedException('Token has been expired');
+        }
+    }
+
+    /**
+     * @param string $token
+     * @return int
+     * @throws NotFoundException
+     */
+    private function getUserIdByToken(string $token): int
+    {
+        $tokenData = $this->tokenArray[$token] ?? '';
+        if ($tokenData !== '') {
+            return $tokenData['userId'];
+        }
+        throw new NotFoundException(
+            sprintf(
+                'User not found by token %s',
+                $token
+            )
+        );
     }
 
     /**
@@ -125,5 +208,32 @@ class Api {
         ],
             404
         );
+    }
+
+    /**
+     * @param string $message
+     */
+    public function internalServerErrorResponse(string $message = ''): void
+    {
+        $data = [
+            'error' => 'Internal Server Error'
+        ];
+        if ($message !== '') {
+            $data['message'] = $message;
+        }
+        $this->formatResponse(
+            $data,
+            500
+        );
+    }
+
+    private function getUserId($email, $password): int
+    {
+        $data = [
+            '' => [
+                'password' => '',
+
+            ]
+        ];
     }
 }
