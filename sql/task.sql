9fdba3c46b0c942c7d292416e43dcf29cfd4a653
Sergei Matros <sergei@matros.com.ua>
2019-05-07 00:17:04 +0300

    DDL

--- sql/task.sql
+++ sql/task.sql
@@ -1,9 +1,10 @@
-CREATE TABLE `task` (
+CREATE TABLE `task2` (
   `id` INT(11) AUTO_INCREMENT,
   `title` VARCHAR(64) NOT NULL,
   `userId` INT(11) NOT NULL,
   `status` TINYINT NOT NULL,
   `priority` TINYINT NOT NULL,
   `dueDate` INT(11) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY (`dueDate`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
